f5343b83a1cd40e281bd13797d9a9f694a77c4e5
krystiano13 <crystianotv@gmail.com>
2024-04-27 14:31:23 +0200

    implement redirect functions for logged in and not logged in users

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,4 +1,11 @@
 class TaskController < ApplicationController
-  before_action :authenticate_user!
+  before_action :check_user
   def index; end
+
+  private
+  def check_user
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
