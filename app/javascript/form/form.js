f7c0d16114601f29f6b506708668089987e17863
krystiano13 <crystianotv@gmail.com>
2024-04-28 12:35:41 +0200

    implement toggling edit and create forms

---
+++ app/javascript/form/form.js
@@ -0,0 +1,29 @@
+const createForm = document.querySelector("#create");
+const editForm = document.querySelector("#edit");
+const cancelButton = document.querySelector("#cancel");
+const tasks = document.querySelectorAll(".task");
+
+let edit = false;
+
+function toggleForms() {
+    if(edit) {
+        editForm.classList.remove("hidden");
+        createForm.classList.add("hidden");
+    }
+    else {
+        editForm.classList.add("hidden");
+        createForm.classList.remove("hidden");
+    }
+}
+
+cancelButton.addEventListener("click",() => {
+    edit = false;
+    toggleForms();
+})
+
+tasks.forEach(item => {
+    item.addEventListener("click", () => {
+        edit = true;
+        toggleForms();
+    })
+});
