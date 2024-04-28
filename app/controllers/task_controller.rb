2f1ecec789c1ef2a24a7527d53340c47bccbe1cd
krystiano13 <crystianotv@gmail.com>
2024-04-28 12:12:53 +0200

    implement creating tasks

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -6,13 +6,26 @@ class TaskController < ApplicationController
   end
 
   def create
+    task = Task.new(get_task_params)
+    task.done = false
+    task.user = current_user.email
 
+    if task.save
+      redirect_to tasks_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def destroy
     id = params[:id]
     Task.destroy(id)
     redirect_to tasks_path
+  end
+
+  private
+  def get_task_params
+    params.require(:task).permit(:text);
   end
 
   private
